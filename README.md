06ccdd60ced4f7007024abf2666213e16cd92bb6
ranamutta <sarirana98@gmail.com>
2024-01-16 13:55:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>NETFLIX-PROJECT</h1>
+
+Özellikler
+
+-Popüler Filmleri Görüntüleme: En popüler filmleri ana sayfada gözden geçirme.
+
+-Farklı Kategorilerde Filmleri Görme: Aksiyon, komedi, dram gibi farklı kategorilerdeki filmleri görüntüleme.
+
+-Film Detaylarına Erişme: Oyuncular, gelir, bütçe, yapım şirketi, dil gibi detayları inceleme.
+
+Film Fragmanlarını İzleme: Filmlerin fragmanlarını görüntüleme.
+
+
+<img src="netflix.gif"/>
---
+++ src/constant.js
@@ -0,0 +1,9 @@
+export const options = {
+       
+       headers: {
+         accept: 'application/json',
+         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWZhYzExODc2NzI5MzQ5MTAzMTZhNmEwYTEzM2MxZCIsInN1YiI6IjY1YTU3YTU1ODI4OWEwMDBjNmFkYjE2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NWiwI_lyBzrzgHEAqgBlXQn8iRs-8LrvPPdsLraos2U'
+       }
+     };
+
+     export const baseImgURL = 'https://image.tmdb.org/t/p/original';    
